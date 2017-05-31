7d9a70dc5245ce507791f358a8e329c7f72f8847
SimasRug <simonas.rugevicius@gmail.com>
2017-05-31 23:57:08 +0200

    fixed images and styling

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-
+innerHeight:any;
+innerWidth: any;
+innerHalfHeight: any;
   items = [
     {class:'linkedin', img:'../../assets/img/linkedin.png', link:'https://www.linkedin.com/in/simonas-rugevicius-4267469b/'},
     {class:'facebook', img:'../../assets/img/facebook.png', link:'https://www.facebook.com/simas.rug'},
@@ -16,7 +19,14 @@ export class ContactComponent implements OnInit {
     {class:'email', img:'../../assets/img/email.png', link:'mailto:simonas.rugevicius@gmail.com'},
   ];
 
-  constructor() { }
+  constructor() {
+    this.innerHeight = (window.screen.height);
+    this.innerWidth = (window.screen.width);
+    console.log(this.innerHeight, this.innerWidth);
+    this.innerHalfHeight = (this.innerHeight/2)-54;
+    console.log(this.innerHalfHeight);
+
+  }
 
   ngOnInit() {
   }
